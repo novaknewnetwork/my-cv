8179950bc0907e52225ab4814bbbf945583161e1
Oleksandr <novaknewnetwork@gmail.com>
2023-04-17 12:25:15 +0300

    edit some files

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ const HeaderSocials = () => {
   return (
     <div className="header__socials">
         <a href="https://www.linkedin.com/in/aleksandr-novak-891474257" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com/novak-new-network" target="_blank"><BsGithub/></a>
+        <a href="https://github.com/novaknewnetwork" target="_blank"><BsGithub/></a>
         <a href="https://www.upwork.com/freelancers/~018dc4755325ab5348?viewMode=1" target="_blank"><SiUpwork/></a>
     </div>
   )
